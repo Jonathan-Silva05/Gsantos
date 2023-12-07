5ab08189ce69872382ccb5dde19aa23430733d5d
Jonathan-Silva05 <133710242+Jonathan-Silva05@users.noreply.github.com>
2023-12-07 13:44:45 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
 
+
+    function mensage1() {
+        var element = document.getElementById("mensage-1");
+        element.classList.add("contentMensage-1");
+    }
+    function mensage2() {
+        var element = document.getElementById("mensage-2");
+        element.classList.add("contentMensage-2");
+    }
+    function mensage3() {
+        var element = document.getElementById("mensage-3");
+        element.classList.add("contentMensage-3");
+    }
+    function mensage4() {
+        var element = document.getElementById("mensage-4");
+        element.classList.add("contentMensage-4");
+    }
+    function mensage5() {
+        var element = document.getElementById("mensage-5");
+        element.classList.add("contentMensage-5");
+    }
+    function mensage6() {
+        var element = document.getElementById("mensage-6");
+        element.classList.add("contentMensage-6");
+    }
+    function mensage7() {
+        var element = document.getElementById("mensage-7");
+        element.classList.add("contentMensage-7");
+    }
+    function mensage8() {
+        var element = document.getElementById("mensage-8");
+        element.classList.add("contentMensage-8");
+    }
+    function mensage9() {
+        var element = document.getElementById("mensage-9");
+        element.classList.add("contentMensage-9");
+    }
